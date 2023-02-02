ce859cea2e62313d2192dfcd9f7a790d72960ae4
janphoffmann <59919531+janphoffmann@users.noreply.github.com>
2023-02-02 17:43:47 +0100

    Update AsyncTelnet.ino

--- example/AsyncTelnet.ino
+++ example/AsyncTelnet.ino
@@ -1,16 +1,20 @@
 
 #include "AsyncTelnet.h"
 
-AsyncTelnet Test();
+AsyncTelnet Test(); // AsyncTelnet::AsyncTelnet(uint16_t port = 23);
 
+// callback function when data is received by telnet
 void data_cb_f(const char *data, size_t len){
   Serial.write(data,len);
 }
+
+// callback function when Telnet client is connected
 void conn_cb_f(void*, AsyncClient* c){
   Serial.print("Client connected");
 }
 
 void setup() {
+  // standard wifi connection
   WiFi.disconnect(true, true);
   delay(200);
   WiFi.mode(WIFI_STA);
@@ -18,6 +22,7 @@ void setup() {
   if (WiFi.waitForConnectResult() != WL_CONNECTED) return;
   WiFi.setAutoReconnect(true);
 
+  // attach callbacks first, before the begin function
   Test.onConnect(conn_cb_f);
   Test.onTelnetResponse(data_cb_f);
   Test.begin(); // AsyncTelnet::begin(bool checkConnection = true, bool mDNS = false);
