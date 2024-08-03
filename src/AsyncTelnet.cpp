96d2bdcbb80f476b673d3ef64bf27ea80acc3202
tomasz90 <49351206+tomasz90@users.noreply.github.com>
2024-08-03 21:04:51 +0200

    Update AsyncTelnet.cpp

--- src/AsyncTelnet.cpp
+++ src/AsyncTelnet.cpp
@@ -1,83 +1,90 @@
 #include "AsyncTelnet.h"
 
 
-AsyncTelnet::AsyncTelnet(uint16_t port) : server(port) { 
-  server_port = port;
+AsyncTelnet::AsyncTelnet(uint16_t port) : server(port) {
+    server_port = port;
 }
 
 bool AsyncTelnet::begin(bool checkConnection /* = true */, bool mDNS) {
-  if (checkConnection) {
-    // connected to WiFi or is ESP in AP mode?
-    if (WiFi.status() != WL_CONNECTED) return false;
-    if(mDNS){
-      if (!MDNS.begin("esp32")) {
-        Serial.println("Error setting up MDNS responder!");
-        while(1) {
-            delay(1000);
+    if (checkConnection) {
+        // connected to WiFi or is ESP in AP mode?
+        if (WiFi.status() != WL_CONNECTED) return false;
+        if (mDNS) {
+            if (!MDNS.begin("esp32")) {
+                Serial.println("Error setting up MDNS responder!");
+                while (1) {
+                    delay(1000);
+                }
+            }
+            MDNS.addService("http", "tcp", 80);
         }
-      }
-      MDNS.addService("http", "tcp", 80);
     }
-  }
-  server.onClient([=](void *, AsyncClient* c){
-    if(c == NULL) return;
-    client = c;
-    // client->setRxTimeout(10);
-    ip = client->remoteIP();
-    if(on_connect != NULL) on_connect(NULL, client);
-    c->onDisconnect([=](void *, AsyncClient* c){ 
-      if(on_disconnect != NULL) on_disconnect(c); 
-      c->close(true);
-      c->free();
-      delete c;
+    server.onClient([=](void *, AsyncClient *c) {
+        if (c == NULL) return;
+        client = c;
+        // client->setRxTimeout(10);
+        ip = client->remoteIP();
+        if (on_connect != NULL) on_connect(NULL, client);
+        c->onDisconnect([=](void *, AsyncClient *c) {
+            if (on_disconnect != NULL) on_disconnect(c);
+            c->close(true);
+            c->free();
+            delete c;
+        }, this);
+        if (on_incoming_data != NULL) {
+            c->onData([=](void *, AsyncClient *, void *data, size_t len) {
+                buffer.append((const char *) data, len);
+
+                if (buffer.find('\n') != std::string::npos) {
+                    write("\r\n");
+                    on_incoming_data(buffer);
+                    buffer.clear();
+                }
+            }, this);
+        }
+        client->setNoDelay(true);
     }, this);
-    if(on_data != NULL){
-      c->onData([=](void *, AsyncClient* , void *data, size_t len){ 
-        // Serial.println("data incoming");
-        on_data((const char *)data, len); 
-      }, this);      
-    }
-    client->setNoDelay(true);
-  }, this);
-  server.setNoDelay(true);
-  server.begin();
-  return true;
+    server.setNoDelay(true);
+    server.begin();
+    return true;
 }
 
-void AsyncTelnet::stop() { 
-  server.end(); 
+void AsyncTelnet::stop() {
+    server.end();
 }
 
-bool AsyncTelnet::isClientConnected(AsyncClient *Client){
-  return (Client->connected());
+bool AsyncTelnet::isClientConnected(AsyncClient *Client) {
+    return (Client->connected());
 }
 
 void AsyncTelnet::disconnectClient() {
-  client->stop();
-  isConnected = false;
+    client->stop();
+    isConnected = false;
 }
 
-size_t AsyncTelnet::write(const char* data) {
-    if(data == NULL) {
+size_t AsyncTelnet::write(const char *data) {
+    if (data == NULL) {
         return 0;
     }
     return client->write(data, strlen(data));
 }
 
-size_t AsyncTelnet::write(const char* data, size_t size, uint8_t apiflags) {
+size_t AsyncTelnet::write(const char *data, size_t size, uint8_t apiflags) {
     size_t will_send = -client->add(data, size, apiflags);
-    if(!will_send || !client->send()) {
+    if (!will_send || !client->send()) {
         return 0;
     }
     return will_send;
 }
 
-void AsyncTelnet::onConnect(ConnHandler callbackFunc) { 
-  on_connect = callbackFunc; 
+void AsyncTelnet::onConnect(ConnHandler callbackFunc) {
+    on_connect = callbackFunc;
 }
-void AsyncTelnet::onDisconnect(DisconnHandler callbackFunc) { 
-  on_disconnect = callbackFunc; 
+
+void AsyncTelnet::onDisconnect(DisconnHandler callbackFunc) {
+    on_disconnect = callbackFunc;
 }
-void AsyncTelnet::onTelnetResponse(ResponseHandler callbackFunc) { 
-  on_data = callbackFunc; 
+
+void AsyncTelnet::onIncomingData(IncomingDataHandler callbackFunc) {
+    on_incoming_data = callbackFunc;
 }
